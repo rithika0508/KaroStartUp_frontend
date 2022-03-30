a9fdc92c2216833b8549e91cbb8dec3150aab581
Subhashree126 <101786841+Subhashree126@users.noreply.github.com>
2022-03-30 21:48:33 +0530

    Add files via upload

---
+++ src/components/Home_Page/Home.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import HomeImage from './Home.png'
+import './home.css'
+import logo from './logo.png'
+
+export default function Home() {
+  return (
+       <> <div className="main">
+        <div className="row">
+        <div className="col-lg-4 col-md-12 mx-5">
+                <img className="HomeImg" src={HomeImage} />
+            </div>
+            <nav className="navbar navbar-expand-lg navbar-dark ">
+                <div className="container-fluid">
+                  <a className="navbar-brand" href="#"><img classname="logo" src={logo} /></a>
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    
+                    <ul className="nav">
+                        <li className="nav-item">
+                          <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">Rewards</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">About Us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Register</a>
+                        </li>
+                      </ul>
+                  </div>
+                </div>
+              </nav>
+        </div>
+        
+        <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-lg-4 col-md-12">
+                <p className="head my-5">Bridging India’s Skill Gap through Live classnameroom and Internship opportunities.</p>
+                <button className="regButton ">Register</button>
+            </div>
+            
+        </div>
+        
+        
+    </div>
+
+    
+    </>
+  )
+}
