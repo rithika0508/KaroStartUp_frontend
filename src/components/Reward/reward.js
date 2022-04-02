0d042d1c651c950a094c277bfbd3fbb261994c82
SiddharthVartha <siddharthvartha@gmail.com>
2022-04-02 13:02:15 +0530

    Error Solved Reward

--- src/components/Reward/reward.js
+++ src/components/Reward/reward.js
@@ -1,65 +1,65 @@
-import "./reward.module.css";
+import styles from "./reward.module.css";
 import bgimage from './plain-blue-01.jpg';
 import wallpaper from './wallpaper.jpg';
 
-const Reward=()=> {
-    
-    return (<>
-        <div className="hea">Heading</div>
+const Reward = () => {
+
+  return (<>
+    <div className={styles.hea}>Heading</div>
 
 
-<div className="wrap">
+    <div className={styles.wrap}>
 
 
-  <div className="move move_original" style={{marginLeft:"0px"}}>
+      <div className={styles.move} style={{ marginLeft: "0px" }}>
 
-    <a href="http://google.com" target="_blank">
-      <div className="car internship">
-        <img src={wallpaper} alt="Internship Opportunities" className="image"></img>
+        <a href="http://google.com"  rel="noopener">
+          <div className={styles.car}>
+            <img src={wallpaper} alt="Internship Opportunities" className={styles.image}></img>
+          </div>
+        </a>
+        <a href="http://google.com"  rel="noopener">
+          <div className={styles.car}>
+            <img src={wallpaper} alt="Discount Coupons" className={styles.image}></img>
+          </div>
+        </a>
+        <a href="http://google.com"  rel="noopener">
+          <div className={styles.car}>
+            <img src={wallpaper} alt="Letter of Recommendation" className={styles.image}></img>
+          </div>
+        </a>
+        <a href="http://google.com"  rel="noopener">
+          <div className={styles.car}>
+            <img src={wallpaper} alt="Video Courses" className={styles.image}></img>
+          </div>
+        </a>
+
+
+        <a href="http://google.com"  rel="noopener">
+          <div className={styles.car}>
+            <img src={wallpaper} alt="Internship Opportunities" className={styles.image}></img>
+          </div>
+        </a>
+        <a href="http://google.com"  rel="noopener">
+          <div className={styles.car}>
+            <img src={wallpaper} alt="Discount Coupons" className={styles.image}></img>
+          </div>
+        </a>
+        <a href="http://google.com"  rel="noopener">
+          <div className={styles.car}>
+            <img src={wallpaper} alt="Letter of Recommendation" className={styles.image}></img>
+          </div>
+        </a>
+        <a href="http://google.com"  rel="noopener">
+          <div className={styles.car}>
+            <img src={wallpaper} alt="Video Courses" className={styles.image}></img>
+          </div>
+        </a>
+
       </div>
-    </a>
-    <a href="http://google.com" target="_blank">
-      <div className="car discount">
-        <img src={wallpaper} alt="Discount Coupons" className="image"></img>
-      </div>
-    </a>
-    <a href="http://google.com" target="_blank">
-      <div className="car letter">
-        <img src={wallpaper} alt="Letter of Recommendation" className="image"></img>
-      </div>
-    </a>
-    <a href="http://google.com" target="_blank">
-      <div className="car courses">
-        <img src={wallpaper} alt="Video Courses" className="image"></img>
-      </div>
-    </a>
+    </div>
 
-
-    <a href="http://google.com" target="_blank">
-      <div className="car internship">
-        <img src={wallpaper} alt="Internship Opportunities" className="image"></img>
-      </div>
-    </a>
-    <a href="http://google.com" target="_blank">
-      <div className="car discount">
-        <img src={wallpaper} alt="Discount Coupons" className="image"></img>
-      </div>
-    </a>
-    <a href="http://google.com" target="_blank">
-      <div className="car letter">
-        <img src={wallpaper} alt="Letter of Recommendation" className="image"></img>
-      </div>
-    </a>
-    <a href="http://google.com" target="_blank">
-      <div className="car courses">
-        <img src={wallpaper} alt="Video Courses" className="image"></img>
-      </div>
-    </a>
-
-  </div>
-</div>
-
-    </>);
+  </>);
 }
 
 export default Reward;
