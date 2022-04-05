2bc13e97c772aa3541b87f17a14f8c886eef0b47
SiddharthVartha <siddharthvartha@gmail.com>
2022-04-05 22:28:45 +0530

    AboutUs

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Register from "./components/Register/Register";
 import Level from "./components/LvlSystem/Levels";
 import ChatBot from "./components/ChatBot/ChatBot";
 import Home from "./components/Home_Page/Home";
+import About from "./components/AboutUs/About";
 function App() {
   const [visibility, setVisibility] = useState(false);
   return (
     <>
       <Register visibility={visibility} setVisibility={setVisibility} />
       <Level visibility={visibility} setVisibility={setVisibility} />
+      <About/>
     </>
   );
 }
